6459315ed3e71e64291fc0a9c4aeca0a9003f1f6
Akif Yusein <akif05@gmail.com>
2018-07-03 11:27:46 +0100

    Tetsting exec to execute command

--- modules/percona/manifests/init.pp
+++ modules/percona/manifests/init.pp
@@ -43,12 +43,14 @@
 # Copyright 2018 Your name here, unless otherwise noted.
 # https://repo.percona.com/apt/percona-release_0.1-6.wheezy_all.deb
 #
+#$root_pass = "serv\!\$ion"
 
 include apt
 
-  apt::source { 'percona-release':
+apt::source { 'percona-release':
     location => 'http://repo.percona.com/apt',
-    release  => 'percona-release_0.1-6.xenial_all.deb',
+    #   release  => 'percona-release_0.1-6.xenial_all.deb',
+    release  => '0.1-6.xenial_all.deb',
     repos    => 'main',
     key      => {
     id       => '4D1BB29D63D98E422B2113B19334A25F8507EFA5',
@@ -60,8 +62,19 @@ include apt
       },
 }
 
-  class percona {
+class percona {
   include percona::config
   include percona::install
   include percona::service
+
+  #  exec { 'set mysql root pass':
+  #   command =>  'mysqladmin -u root password ${root_pass}',
+  #   path    => ['/usr/bin/', '/usr/sbin/',],
+  # }
+
+  #exec { 'mysqladmin -u root -p"serv!$ion" password "newpassword"':
+  #exec { 'mysqladmin -u root  password $root_pass"':
+  #  path => ['/usr/bin/', '/usr/sbin/',],
+  #}
+
 }
